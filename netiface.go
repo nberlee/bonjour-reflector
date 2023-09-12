5fe4b1d1ac0612f22332142fb3e5eadd0b22103e
Nico Berlee <nico.berlee@on2it.net>
2023-09-12 14:46:39 +0200

    fix: only disable rx-vlan-filter when enabled

--- netiface.go
+++ netiface.go
@@ -13,6 +13,17 @@ func removeVlanFilter(iface string) {
 	}
 	defer ethHandle.Close()
 
+	features, err := ethHandle.Features(iface)
+	if err != nil {
+		slog.Error("Unable to get features for interface", iface, err)
+		return
+	}
+	if !features["rx-vlan-filter"] {
+		return
+	}
+
+	slog.Info("Hardware vlan filter (rx-vlan-filter) is enabled, disabling it")
+
 	err = ethHandle.Change(iface, map[string]bool{
 		"rx-vlan-filter": false,
 	})
